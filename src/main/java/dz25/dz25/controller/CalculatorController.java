993eb26c24ed0f1090ba21d0ddfb6d0c20dee307
Evllooew <evllooev@icloud.com>
2023-11-06 17:18:54 +0300

    дз готово

---
+++ src/main/java/dz25/dz25/controller/CalculatorController.java
@@ -0,0 +1,42 @@
+package dz25.dz25.controller;
+
+import dz25.dz25.service.ServiceInterface;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/calculator")
+public class CalculatorController {
+    ServiceInterface serviceInterface;
+
+    @GetMapping
+    public String sayHello() {
+        return "Welcome to calculator";
+    }
+
+    @GetMapping("/plus")
+    public String makeSum(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + "+" + num2 + "=" + serviceInterface.sum(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String makeSubtraction(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + "-" + num2 + "=" + serviceInterface.subtraction(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String makeMultiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + "*" + num2 + "=" + serviceInterface.multiply(num1, num2);
+
+    }
+
+    @GetMapping("/divide")
+    public String makeDivide(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + "/" + num2 + "=" + serviceInterface.divide(num1, num2);
+    }
+}
+
